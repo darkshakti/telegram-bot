1881cc5963a40947541e8816865cb30d31e44b4e
margarita.vladyko <margarita.vladyko@itglobal.com>
2024-09-11 08:50:07 +0300

    add date

--- src/main.js
+++ src/main.js
@@ -43,9 +43,60 @@ class MyTelegramBot extends HtmlTelegramBot {
         await this.sendText(answer)
     }
 
+    async date() {
+        this.mode = 'date'
+        const text = this.loadMessage('date')
+        await this.sendImage('date')
+        await this.sendTextButtons(text, {
+            'date_grande': 'Ариана Гранде',
+            'date_lawrence': 'Дженнифер Лоуренс',
+            'date_robbie': 'Марго Робби',
+            'date_zendaya': 'Зендея',
+            'date_gosling': 'Райан Гослинг',
+            'date_hardy': 'Том Харди',
+        })
+    }
+
+    async dateButton(callbackQuery) {
+        const query = callbackQuery.data;
+        await this.sendText(query);
+        await this.sendImage(query)
+        await this.sendText('Отличный выбор! Пригласи девушку/парня на свидание за 5 сообщений:')
+        const prompt = this.loadPrompt(query)
+        await chatgpt.setPrompt(prompt);
+    }
+
+    async dateDialog(msg) {
+        const text = msg.text;
+        const myMessage = await this.sendText('Девушка набирает текст...')
+        const answer = await chatgpt.addMessage(text);
+        await this.editText(myMessage, answer);
+    }
+
+    async message(msg) {
+        this.mode = 'message';
+        const text = this.loadMessage('message');
+        await this.sendImage('message');
+        await this.sendText(text);
+    }
+
+    async messageButton(callbackQuery) {
+
+    }
+
+    async messageDialog(msg) {
+
+    }
+
+    async
+
     async hello(msg){
         if (this.mode === 'gpt')
             await this.gptDialog(msg)
+        else if (this.mode === 'date')
+            await this.dateDialog(msg)
+        else if (this.mode === 'message')
+            await this.messageDialog(msg)
         else {
             const text = msg.text;
             await this.sendText('<b>Привет!</b>')
@@ -78,5 +129,10 @@ const bot = new MyTelegramBot('7515364389:AAG8j4mpKksYrWcL-HRR9-5FGtgjQGtkAQo');
 bot.onCommand(/\/start/, bot.start); // /start
 bot.onCommand(/\/html/, bot.html); // /html
 bot.onCommand(/\/gpt/, bot.gpt); // /gpt
+bot.onCommand(/\/date/, bot.date); // /date
+bot.onCommand(/\/message/, bot.message); // /message
+
 bot.onTextMessage(bot.hello);
-bot.onButtonCallback(/^.*/, bot.helloButton);
+bot.onButtonCallback(/^date_.*/, bot.dateButton);
+bot.onButtonCallback(/^date_.*/, bot.dateButton);
+bot.onButtonCallback(/^.*/, bot.messageButton);
